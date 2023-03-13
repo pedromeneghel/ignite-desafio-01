1cae41204c7da33f2bdd2df540a7e19d013df9f8
Pedro Meneghel <pedromeneghel@gmail.com>
2023-03-13 16:16:16 -0300

    refactor: remove console.log desnecessário

--- src/server.js
+++ src/server.js
@@ -16,8 +16,6 @@ const server = http.createServer(async (request, response) => {
   if (route) {
     const routeParams = request.url.match(route.path);
 
-    console.log("routeParams.groups", routeParams.groups);
-
     const { query, ...params } = routeParams.groups;
 
     request.params = params;
